0302a8ad22a77f60ee3c0642bb6b3c56885e109a
junchh <cjunho9868@gmail.com>
2020-11-07 17:10:57 +0700

    kinda passsed simple lock

--- txn/lock_manager.cc
+++ txn/lock_manager.cc
@@ -20,14 +20,15 @@ bool LockManagerA::WriteLock(Txn* txn, const Key& key) {
 
   // kalo gak ada di lock table
   if(this->lock_table_.find(key) == this->lock_table_.end()) {
-    deque<LockRequest> h;
-    h.push_back(lr);
-    this->lock_table_[key] = &h;
+    deque<LockRequest> *h = new deque<LockRequest>();
+    h->push_back(lr);
+    this->lock_table_[key] = h;
     return true;
   } else {
 
-    // udah ada yg di queue, jadi gak bisa langsung granted
+    // udah ada yg di queue, bisa granted kalau kosong
     this->lock_table_[key]->push_back(lr);
+    if(this->lock_table_[key]->size() == 1) return true;
 
     if(this->txn_waits_.find(txn) == this->txn_waits_.end()) {
       this->txn_waits_[txn] = 1;
@@ -49,13 +50,42 @@ void LockManagerA::Release(Txn* txn, const Key& key) {
   // CPSC 438/538:
   //
   // Implement this method!
+
+  //cek apakah front merupakan txn yang akan di release
+  if(this->lock_table_[key]->front().txn_ == txn) {
+    this->lock_table_[key]->pop_front();
+
+    if(this->lock_table_[key]->size() > 0) {
+
+      // append next transaction di key yang sama ke ready txn
+      this->ready_txns_->push_back(this->lock_table_[key]->front().txn_);
+    }
+  } else {
+    auto it = this->lock_table_[key]->begin();
+    while(it->txn_ != txn) {
+      it++;
+    }
+
+    this->lock_table_[key]->erase(it);
+  }
 }
 
 LockMode LockManagerA::Status(const Key& key, vector<Txn*>* owners) {
   // CPSC 438/538:
   //
   // Implement this method!
-  return UNLOCKED;
+
+  owners->clear();
+
+  if(this->lock_table_[key]->size() > 0) {
+      owners->push_back(this->lock_table_[key]->front().txn_);
+  }
+
+  if(owners->empty()) {
+    return UNLOCKED;
+  } else {
+    return EXCLUSIVE;
+  }
 }
 
 LockManagerB::LockManagerB(deque<Txn*>* ready_txns) {
